83dccdb8b392c998a3daf413c918e88b6ac6c1ff
Jaime Burbano <burbanojaime98@gmail.com>
2018-04-25 13:36:22 -0500

    No funciona

---
+++ index.js
@@ -0,0 +1,37 @@
+const MongoClient = require('mongodb').MongoClient,
+    express = require('express'),
+    engines = require('consolidate');
+
+var app = express(),
+    db;
+
+app.engine('hbs', engines.handlebars);
+
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'));
+
+
+// Conectarse a Base de Datos
+MongoClient.connect('mongodb://localhost:27017', function (err, client) {
+    if (err) throw err;
+
+    db = client.db('');
+
+    // Iniciar servidor
+    app.listen(5000);
+});
+
+
+app.get('/productos', (req, res) => {
+    db.collection('carros').find().toArray((err, result) => {
+            res.render('prod', {
+                productos: result
+            });
+        })
+});
+
+/*app.get('/carros/:id', (req, res) => {
+    db.collection('carros').find({ modelo: req.params.id }).toArray((err, result) => res.send(result))
+*/
