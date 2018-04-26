c21fb01bcdb2e62bcd46b017ab947baa357c9b1d
Jaime Burbano <burbanojaime98@gmail.com>
2018-04-26 15:42:22 -0500

    ya funciona

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.static('public'));
 MongoClient.connect('mongodb://localhost:27017', function (err, client) {
     if (err) throw err;
 
-    db = client.db('');
+    db = client.db('TallerDos');
 
     // Iniciar servidor
     app.listen(5000);
@@ -25,12 +25,34 @@ MongoClient.connect('mongodb://localhost:27017', function (err, client) {
 
 
 app.get('/productos', (req, res) => {
-    db.collection('carros').find().toArray((err, result) => {
-            res.render('index', {
-                productos: result
-            });
-        })
+    var carroCollection = db.collection('carros').find();
+    
+    if(req.query.marca){
+        carroCollection.filter({marca: req.query.marca});
+    }
+    carroCollection.toArray((err, result) => {
+
+        res.render('productos', {
+            carros: result
+        });
+    })
 });
+
+app.get('/productos/', (req, res) => {
+    var carroCollection = db.collection('carros').find();
+    
+    if(req.query.marca){
+        carroCollection.filter({marca: req.query.marca});
+    }
+    carroCollection.toArray((err, result) => {
+
+        res.render('productos', {
+            carros: result
+        });
+    })
+});
+
+
 
 /*app.get('/carros/:id', (req, res) => {
     db.collection('carros').find({ modelo: req.params.id }).toArray((err, result) => res.send(result))
